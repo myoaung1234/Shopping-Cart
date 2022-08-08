761133fb535f81112ca39a73739facd119b77dba
myoaung1234 <108474627+myoaung1234@users.noreply.github.com>
2022-08-09 01:58:27 +0630

    first commit

---
+++ src/Data.js
@@ -0,0 +1,31 @@
+let shopItemsData = [{
+    id: "a",
+    name: "Causual Shirt",
+    price: 45,
+    desc: "Lorem ipum dolor, sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg"
+},{
+    id: "b",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipum dolor, sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg"
+},{
+    id: "c",
+    name: "T-Shirt",
+    price: 25,
+    desc: "Lorem ipum dolor, sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg"
+},{
+    id: "d",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipum dolor, sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg"
+},{
+    id: "e",
+    name: "Mens Tie",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-5.png",
+  }];
